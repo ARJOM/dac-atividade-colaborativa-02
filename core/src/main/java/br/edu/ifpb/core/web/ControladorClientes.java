47281a99075053afe792d409832dcb10e9d4485a
Antônio Ricart <ricarteusuo11@gmail.com>
2021-09-23 10:20:56 -0300

    fix: corrige escopo do controlador de clientes

--- core/src/main/java/br/edu/ifpb/core/web/ControladorClientes.java
+++ core/src/main/java/br/edu/ifpb/core/web/ControladorClientes.java
@@ -2,14 +2,16 @@ package br.edu.ifpb.core.web;
 
 import br.edu.ifpb.core.domain.cliente.*;
 
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
 import java.util.List;
 
 @Named // Use @javax.faces.bean.ManagedBean on outdated environments.
-@RequestScoped // Use @javax.faces.bean.RequestScoped on outdated environments.
-public class ControladorClientes {
+@SessionScoped // Use @javax.faces.bean.RequestScoped on outdated environments.
+public class ControladorClientes implements Serializable {
 
     @Inject
     private ListaDeClientes listarClientes;
@@ -37,6 +39,8 @@ public class ControladorClientes {
     }
 
     public String submit() {
+        String result = "Cliente{id: "+ cliente.getId() +", nome: "+ cliente.getNome() +", cpf: "+ cliente.getCpf()+"}";
+        System.out.println(result);
         if (cliente.getId()==0){
             this.create();
         }
